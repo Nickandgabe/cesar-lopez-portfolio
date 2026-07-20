b21e472e4711bedff461fdad819948a2ac8e6414
Nickandgabe <braingroupsas@gmail.com>
2026-07-20 10:55:42 -0400

    Add rewrite for /myproductdashboard route

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "standalone",
+    output: "standalone",
+    async rewrites() {
+          return [
+            {
+                      source: "/myproductdashboard",
+                      destination: "/myproductdashboard.html",
+            },
+                ];
+    },
 };
 
 export default nextConfig;
